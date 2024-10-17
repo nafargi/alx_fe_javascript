1ab670a4056da62cdeafe285a99e6f82ce686185
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-17 16:33:13 +0300

    showRandomQuote edited

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -5,42 +5,42 @@ const text = document.getElementById('newQuoteText');
 const category = document.getElementById('newQuoteCategory');
 
 const quotes = [
-    // { 
-    //     text: "The only way to do great work is to love what you do.", 
-    //     category: "Motivation" 
-    //   },
-    //   { 
-    //     text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", 
-    //     category: "Success" 
-    //   },
-    //   { 
-    //     text: "Believe you can and you're halfway there.", 
-    //     category: "Inspiration" 
-    //   },
-    //   { 
-    //     text: "Act as if what you do makes a difference. It does.", 
-    //     category: "Encouragement" 
-    //   },
-    //   { 
-    //     text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.", 
-    //     category: "Self-Development" 
-    //   },
-    //   { 
-    //     text: "Opportunities don't happen, you create them.", 
-    //     category: "Opportunity" 
-    //   },
-    //   { 
-    //     text: "Don't watch the clock; do what it does. Keep going.", 
-    //     category: "Persistence" 
-    //   },
-    //   { 
-    //     text: "The future belongs to those who believe in the beauty of their dreams.", 
-    //     category: "Dreams" 
-    //   },
-    //   { 
-    //     text: "You don't have to be great to start, but you have to start to be great.", 
-    //     category: "Growth" 
-    //   },
+    { 
+        text: "The only way to do great work is to love what you do.", 
+        category: "Motivation" 
+      },
+      { 
+        text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", 
+        category: "Success" 
+      },
+      { 
+        text: "Believe you can and you're halfway there.", 
+        category: "Inspiration" 
+      },
+      { 
+        text: "Act as if what you do makes a difference. It does.", 
+        category: "Encouragement" 
+      },
+      { 
+        text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.", 
+        category: "Self-Development" 
+      },
+      { 
+        text: "Opportunities don't happen, you create them.", 
+        category: "Opportunity" 
+      },
+      { 
+        text: "Don't watch the clock; do what it does. Keep going.", 
+        category: "Persistence" 
+      },
+      { 
+        text: "The future belongs to those who believe in the beauty of their dreams.", 
+        category: "Dreams" 
+      },
+      { 
+        text: "You don't have to be great to start, but you have to start to be great.", 
+        category: "Growth" 
+      },
       { 
         text: "Your time is limited, so don't waste it living someone else's life.", 
         category: "Life" 
@@ -53,11 +53,11 @@ function getRandomQuote() {
     return quotes[randomIndex];
 }
 
-function displayQuote() {
+function showRandomQuote() {
     const quote = getRandomQuote();
    return quoteDisplay.innerHTML = `<p>${quote.text}</p><span>${quote.category}</span>`;
 }
-newQuoteButton.addEventListener('click', displayQuote);
+newQuoteButton.addEventListener('click', showRandomQuote);
 
 function addQuote(){
     let textValue = text.value.trim();
