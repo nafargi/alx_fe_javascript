380217b012ee9421224cc20e5db9d1bf6a7f834a
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-20 13:15:15 +0300

    intitial commit script.js

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -6,42 +6,6 @@ const quotes = JSON.parse(localStorage.getItem('quotes')) || [
     { 
         text: "The only way to do great work is to love what you do.", 
         category: "Motivation" 
-      },
-      { 
-        text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", 
-        category: "Success" 
-      },
-      { 
-        text: "Believe you can and you're halfway there.", 
-        category: "Inspiration" 
-      },
-      { 
-        text: "Act as if what you do makes a difference. It does.", 
-        category: "Encouragement" 
-      },
-      { 
-        text: "What lies behind us and what lies before us are tiny matters compared to what lies within us.", 
-        category: "Self-Development" 
-      },
-      { 
-        text: "Opportunities don't happen, you create them.", 
-        category: "Opportunity" 
-      },
-      { 
-        text: "Don't watch the clock; do what it does. Keep going.", 
-        category: "Persistence" 
-      },
-      { 
-        text: "The future belongs to those who believe in the beauty of their dreams.", 
-        category: "Dreams" 
-      },
-      { 
-        text: "You don't have to be great to start, but you have to start to be great.", 
-        category: "Growth" 
-      },
-      { 
-        text: "Your time is limited, so don't waste it living someone else's life.", 
-        category: "Life" 
       }
 ]
 
